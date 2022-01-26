8d1de31ab0cb78ffce8946fd84d2fcd6a2f0d500
dannylo-gaivota <80546050+dannylo-gaivota@users.noreply.github.com>
2022-01-26 07:03:06 -0300

    chre: change text and ctrs

--- thkx.php
+++ thkx.php
@@ -1,1 +1,23 @@
-<?php include_once('thnk.html'); ?>
+<?
+// KEY FOR PAYMENT APPROOVED
+// wh_i6ORQ8rjCKXmY8Lrk2IT3CUTlANgHy7OP4NjJ
+
+function hmac_signature(array $body, $webHookSecret)
+{
+  $payload = json_encode($body);
+
+  return base64_encode(hash_hmac('sha256', $payload, $webHookSecret, true));
+}
+
+// Calculando a assinatura
+
+$body = [
+  'event' => 'order.created',
+  'time' => '2020-06-20 00:00:00',
+  'resource' => [
+    'id' => 1121333,
+    // Aqui vem todo o payload do resource.
+  ],
+];
+
+$signature = hmac_signature($body, 'wh_i6ORQ8rjCKXmY8Lrk2IT3CUTlANgHy7OP4NjJ');
